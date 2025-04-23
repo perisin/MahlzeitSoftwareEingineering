d2ae5c37ba9b7b6616df3be6b6b8a6118022e2bd
blochmann <blochipixel@gmail.com>
2025-04-23 16:39:07 +0200

    set up raw maui program

---
+++ code/Mahlzeit/Mahlzeit/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Mahlzeit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ code/Mahlzeit/Mahlzeit/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Mahlzeit
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
